86a33c02697ff38cf39ea6cbf1f7a75e4e7c81c9
Eddie Kohler <ekohler@gmail.com>
2017-02-08 15:54:52 -0500

    Add L6 solutions code.

--- l06.v
+++ l06.v
@@ -7,6 +7,7 @@ Import ListNotations.
 
 Section Lecture6.
 
+  (* True iff the list’s elements are in decreasing order *)
   Inductive Dec : list nat -> Prop :=
   | DNil : Dec []
   | DCons : forall a l,
@@ -15,6 +16,7 @@ Section Lecture6.
       Dec (a :: l).
   Hint Constructors Dec.
 
+  (* True iff the list’s elements are in INcreasing order *)
   Inductive Inc : list nat -> Prop :=
   | INil : Inc []
   | ICons : forall a l,
@@ -23,10 +25,15 @@ Section Lecture6.
       Inc (a :: l).
   Hint Constructors Inc.
 
+  (* A fact about appending increasing lists *)
   Lemma Inc_app xs ys:
     Inc xs -> Inc ys ->
     (forall x y, In x xs -> In y ys -> y <= x) ->
     Inc (xs ++ ys).
   Admitted.
+
+
+  Lemma Dec_rev xs: Dec (rev xs) -> Inc xs.
+  Abort.
 
 End Lecture6.
